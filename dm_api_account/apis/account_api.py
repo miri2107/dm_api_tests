4adca6f4a67dce0ffcc6267acfe868633f4146e7
miri2107 <iri.matrunich@gmail.com>
2024-06-08 17:16:20 -0400

    added test put_v1_account to chek changing email

--- dm_api_account/apis/account_api.py
+++ dm_api_account/apis/account_api.py
@@ -42,3 +42,18 @@ class AccountApi:
             headers=headers
         )
         return response
+
+    def put_v1_account_email(
+            self,
+            json_data,
+    ):
+        """
+        Change registered user email
+        :param json_data:
+        :return:
+        """
+        response = requests.put(
+            url=f'{self.host}/v1/account/email',
+            json=json_data
+        )
+        return response
--- tests/functional/put_v1_account/test_put_v1_account.py
+++ tests/functional/put_v1_account/test_put_v1_account.py
@@ -1,42 +1,45 @@
 from json import loads
+import pprint
 
-from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
+from dm_api_account.apis.account_api import AccountApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
-def test_post_v1_account():
-    # ---User registration
+def test_put_v1_account():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'IM_test_user_20'
+    login = 'IM_test_13'
     email = f'{login}@mail.com'
     password = 'pass123456'
+
+    #  -------User registration
 
     json_data = {
         'login': login,
         'email': email,
         'password': password,
     }
+
     response = account_api.post_v1_account(json_data=json_data)
     print(response.status_code, '  - User creation status code')
     print(response.text)
     assert response.status_code == 201, f"User wasn't created {response.json()}"
 
-    # ---Get email from mail server
+    # -------Get mail from mail server
 
     response = mailhog_api.get_api_v2_messages()
     print(response.status_code, ' - Get mails status code')
     assert response.status_code == 200, "Mails not received"
 
-    # ---Get activation token
-    token = get_activation_token_by_login(login, response)
+    #  -------Get activation token
 
+    token = get_activation_token_by_login(login, response)
     assert token is not None, f"Token not received for user {login}"
 
-    # ---User activation
+    #  -------Activate user
     response = account_api.put_v1_account_token(token)
 
     print(response.status_code, ' - User activation status code')
@@ -56,17 +59,62 @@ def test_post_v1_account():
     print(response.text)
     assert response.status_code == 200, "User didn't logged in"
 
+    #  -------Update email
+
+    json_data = {
+        'login': login,
+        'email': f'{login}_upd@mail.com',
+        'password': password,
+    }
+
+    response = account_api.put_v1_account_email(json_data=json_data)
+
+    print(response.status_code, ' - Update email status code')
+    pprint.pprint(response.text)
+    assert response.status_code == 200, "Email was not changed"
+
+    # ---Log in 2 user after changing email
+
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+
+    response = login_api.post_v1_account_login(json_data=json_data)
+
+    print(response.status_code, ' - Login status code for', login, 'after email changing ')
+    print(response.text)
+    assert response.status_code != 200, "User  logged in successfully"
+
+    #  -------Activate user after changing email
+    response = account_api.put_v1_account_token(token)
+
+    print(response.status_code, ' - User activation status code')
+    assert response.status_code == 200, "User not activated"
+
+    # ---Log in 3 user after changing email and new activation
+
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print(response.status_code, ' - Login status code for', login)
+    print(response.text)
+    assert response.status_code == 200, "User didn't logged in"
+
 
 def get_activation_token_by_login(
         login,
         response
 ):
-    token = None
     for item in response.json()['items']:
         user_data = (loads(item['Content']['Body']))
         user_login = user_data['Login']
-
         if user_login == login:
             token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            print(response.status_code, ' - Get activation token status code')
+            print(login, token)
     return token
