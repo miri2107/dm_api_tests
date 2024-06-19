abaefedd8ec8a392ff9f71896d18d6ef5b677526
miri-adv <iri.adv2107@gmail.com>
2024-06-19 12:55:02 -0400

    created fixture in test_post_v1_account

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+from datetime import datetime
 from json import loads
+
+import pytest
 
 from helpers.account_helper import AccountHelper
 from services.dm_api_account import DMApiAccount
@@ -18,18 +22,48 @@ structlog.configure(
     ]
 )
 
-def test_post_v1_account():
-    # ---User registration
-    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
-    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
+@pytest.fixture
+def mailhog_api():
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
     account = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
-    login = 'IM_test_a30'
+    return account
+
+
+@pytest.fixture
+def account_helper(
+        account_api,
+        mailhog_api
+):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+@pytest.fixture
+def prepare_user():
+    now = datetime.now()
+    date = now.strftime("%d_%m_%Y_%H_%M_%S")
+    login = f'IM_test_{date}'
     email = f'{login}@mail.com'
     password = 'pass123456'
-    account_helper.register_new_user(login=login, password=password,email=email)
+    User = namedtuple("User", ["login", "password", "email"])
+    user = User(login=login, password=password, email=email)
+    return user
+
+
+def test_post_v1_account(
+        account_helper,
+        prepare_user
+):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+    # ---User registration
+    account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.user_login(login=login, password=password)
-
-
