6dfa5ad1c526c9ab2933b5ac0845f3e9797a89fa
miri-adv <iri.adv2107@gmail.com>
2024-06-24 16:22:30 -0400

    update test to utilise prepare_user fixture

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,6 +1,13 @@
 def test_get_v1_account_auth(
-        auth_account_helper
+        prepare_user,
+        auth_account_helper,
+
 ):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+    auth_account_helper.register_new_user(login=login, password=password, email=email)
+    auth_account_helper.auth_client(login=login, password=password)
     auth_account_helper.dm_account_api.account_api.get_v1_account()
 
 
